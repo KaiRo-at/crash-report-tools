b01a35f613f787bb85784b6fdb6f9a59588c5bdb
Robert Kaiser <kairo@kairo.at>
2015-03-20 18:35:36 +0200

    remove old Flash betas

--- get-flashversions.php
+++ get-flashversions.php
@@ -52,9 +52,7 @@ if (count($force_dates)) {
 // Flash versions to gather reports for.
 
 $flash_versions = array('16.0.0.305',
-                        '17.0.0.123',
                         '17.0.0.134',
-                        '17.0.0.135',
                         '17.0.0.141');
 
 // for how many days back to get the data
