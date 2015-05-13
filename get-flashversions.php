6ae53bcdc01230bb613cfd74912a0d1419c45083
Robert Kaiser <kairo@kairo.at>
2015-05-13 13:38:42 +0200

    add some new versions that arrived this week

--- get-flashversions.php
+++ get-flashversions.php
@@ -53,7 +53,8 @@ if (count($force_dates)) {
 
 $flash_versions = array('17.0.0.169',
                         '18.0.0.107',
-                        '18.0.0.114');
+                        '18.0.0.114',
+                        '18.0.0.129');
 
 // for how many days back to get the data
 $backlog_days = 7;
